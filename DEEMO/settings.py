669247d8f7ab58ea83026cb4144953aa4316bc38
Steven Verbraeken <sverbraeken60@gmail.com>
2020-11-12 21:37:53 +0100

    DB changed to Postgresql

--- DEEMO/settings.py
+++ DEEMO/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = 'DEEMO.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangodb',
+        'USER': 'djangodb',
+        'PASSWORD': 'horsehorsehorse',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
