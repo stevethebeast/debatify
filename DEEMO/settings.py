841b1ed96e4e9ef52abfd8a5550517df121bbe77
Steven Verbraeken <sverbraeken60@gmail.com>
2020-11-15 19:22:26 +0100

    ddfdf

--- DEEMO/settings.py
+++ DEEMO/settings.py
@@ -97,16 +97,16 @@ WSGI_APPLICATION = 'DEEMO.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangodb',
-        'USER': 'djangodb',
-        'PASSWORD': 'horsehorsehorse',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+#DATABASES = {
+ #   'default': {
+ #       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+ #       'NAME': 'djangodb',
+ #       'USER': 'djangodb',
+ #       'PASSWORD': 'horsehorsehorse',
+ #       'HOST': 'localhost',
+ #       'PORT': '5432',
+ #   }
+#}
 
 
 # Password validation
