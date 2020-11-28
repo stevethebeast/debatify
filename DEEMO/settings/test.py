14b7181b10f0124c5a295f86bd33a1783ccb115f
Steven Verbraeken <sverbraeken60@gmail.com>
2020-11-28 21:36:40 +0100

    Changes for gitlab testing

--- DEEMO/settings/test.py
+++ DEEMO/settings/test.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_secret_setting('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
