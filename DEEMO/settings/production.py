ac52080d465d762bc2819908ab444725c31264fa
Steven Verbraeken <sverbraeken60@gmail.com>
2020-12-05 12:08:06 +0100

    CORS ALLOWED

--- DEEMO/settings/production.py
+++ DEEMO/settings/production.py
@@ -24,6 +24,8 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS')
 
+CORS_ALLOW_ALL_ORIGINS = env('CORS_ALLOW_ALL_ORIGINS')
+
 # Parse database connection url strings like psql://user:pass@127.0.0.1:8458/db
 DATABASES = {
     # read os.environ['DATABASE_URL'] and raises ImproperlyConfigured exception if not found
