2b0741476a165e6a4bfefd910e357e63cc5149fe
dv-rd <steven.verbraeken@decivision.com>
2020-12-30 12:59:58 +0100

    Changed to Mailtrap

--- DEEMO/settings/production.py
+++ DEEMO/settings/production.py
@@ -2,7 +2,7 @@
 Production Settings for Heroku
 """
 
-import environ
+import environ, requests
 
 # If using in your own project, update the project namespace below
 from .base import *
@@ -31,21 +31,13 @@ DOMAIN = env('DOMAIN')
 
 GOOGLE_RECAPTCHA_SECRET_KEY = env("GOOGLE_RECAPTCHA_SECRET_KEY")
 
-MAILGUN_API_KEY = env("MAILGUN_API_KEY")
-MAILGUN_DOMAIN = env("MAILGUN_DOMAIN")
-MAILGUN_PUBLIC_KEY = env("MAILGUN_PUBLIC_KEY")
-MAILGUN_SMTP_LOGIN = env("MAILGUN_SMTP_LOGIN")
-MAILGUN_SMTP_PASSWORD = env("MAILGUN_SMTP_PASSWORD")
-MAILGUN_SMTP_PORT = env("MAILGUN_SMTP_PORT")
-MAILGUN_SMTP_SERVER = env("MAILGUN_SMTP_SERVER")
-DEFAULT_FROM_EMAIL = env("MAILGUN_SMTP_LOGIN")
-SERVER_EMAIL = env("MAILGUN_SMTP_LOGIN")
-EMAIL_HOST = env("MAILGUN_DOMAIN")
-#EMAIL_USE_TLS = True
-#EMAIL_USE_SSL = False
-EMAIL_PORT = env("MAILGUN_SMTP_PORT")
-EMAIL_HOST_USER = env("MAILGUN_SMTP_LOGIN")
-EMAIL_HOST_PASSWORD = env("MAILGUN_SMTP_PASSWORD")
+response = requests.get("https://mailtrap.io/api/v1/inboxes.json?api_token=" + env("MAILTRAP_API_TOKEN"))
+credentials = response.json()[0]
+EMAIL_HOST = credentials['domain']
+EMAIL_HOST_USER = credentials['username']
+EMAIL_HOST_PASSWORD = credentials['password']
+EMAIL_PORT = credentials['smtp_ports'][0]
+EMAIL_USE_TLS = env("EMAIL_USE_TLS")
 
 # Parse database connection url strings like psql://user:pass@127.0.0.1:8458/db
 DATABASES = {
